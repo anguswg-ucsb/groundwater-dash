f6c48e29e875e7c846f6acbfa32909f84351668f
anguswg-ucsb <anguswatters@gmail.com>
2021-03-28 09:04:50 -0700

    groundwater shiny 3/28 v1

--- app.R
+++ app.R
@@ -4,164 +4,114 @@ today = today_pts(az_time)
 basemap  <-  basemap(today)
 
 ui <- dashboardPage(
-  dashboardHeader(title = "ARIZONA GROUNDWATER WELLS"),
-  dashboardSidebar(disable = TRUE),
-  dashboardBody(
-    fluidRow(
-      column(width = 7,
-             # autocomplete_input("auto", "Search for a County:",
-             #                    value = "",
-             #                    max_options = 5,
-             #                    structure(today$county, names = today$name)),
-             box(width = NULL, solidHeader = TRUE,
-                 leafletOutput("groundMap", height = 650)),
-             box(width = NULL,
-                 title = "Statistics",
-                 status = "primary",
-                 solidHeader = TRUE,
-                 DTOutput('groundTable'))
-             
-      ),
-      
-      column(width = 4,
-             box(width = NULL, status = "primary",
-                 title = "DEPTH TO WATER",
-                 solidHeader = TRUE,
-                 dygraphOutput('groundGraph')),
-             # infoBox("Total cases", icon = icon("credit-card"), fill = TRUE),
-             box(width = NULL, status = "primary",
-                 title = "SECTOR WITHDRAWALS",
-                 solidHeader = TRUE,
-                 plotlyOutput('groundPlot')),
-             valueBoxOutput("depthValue"), 
-             valueBoxOutput("minValue"),
-             valueBoxOutput("maxValue"),
-      )
-    )
-  )
-)
-
-
+     dashboardHeader(title = "USGS Groundwater"),
+     dashboardSidebar(disable = TRUE),
+     dashboardBody(
+         fluidRow(
+             column(width =6,
+                                   # autocomplete_input("auto", "Search for a County:",
+                                     #                    value = "",
+                                     #                    max_options = 5,
+                                     #                    structure(today$county, names = today$name)),
+                                     box(width = NULL, height = 600, solidHeader = TRUE, 
+                                                            leafletOutput("groundMap")),
+                                   # box(width = NULL, status = "primary",
+                                     #     title = "SECTOR WITHDRAWALS",
+                                     #     solidHeader = TRUE,
+                                     #     plotlyOutput('groundPlot'))
+                                     # tabBox(width = NULL, height = 700,
+                                     #        tabPanel("DEPTH TO WATER", dygraphOutput('groundGraph')),
+                                     #        # tabPanel("WITHDRAWALS BY SECTOR", plotlyOutput('groundPlot') ),
+                                     #        tabPanel("CHART", DTOutput('groundTable'))),
+                                     valueBoxOutput("depthValue"),
+                                   valueBoxOutput("minValue"),
+                                   valueBoxOutput("maxValue")
+                                   # box(width = NULL,
+                                     #     title = "Statistics",
+                                     #     status = "primary",
+                                     #     solidHeader = TRUE,
+                                     #     DTOutput('groundTable'))
+                              #        
+                            ),
+       
+               column(width =5,
+                                     box(width = NULL, status = "primary",
+                                                            title = "WITHDRAWALS BY SECTOR",
+                                                            solidHeader = TRUE,
+                                                            plotlyOutput('groundPlot')),
+                                     tabBox(width = NULL,
+                                                             tabPanel("DEPTH TO WATER", dygraphOutput('groundGraph')),
+                                                             # tabPanel("WITHDRAWALS BY SECTOR", plotlyOutput('groundPlot')),
+                                                               tabPanel("TABLE", DTOutput('groundTable'))),
+                              
+                                       # ),
+                            
+                                #       
+                                #        # infoBox("Total cases", icon = icon("credit-card"), fill = TRUE),
+                                # valueBoxOutput("depthValue"), 
+                                # valueBoxOutput("minValue"),
+                                # valueBoxOutput("maxValue")
+                                    
+                                )
+           )
+       )
+   )
 server <- function(input, output, session) {
-  # Global variables initialized
-  wellid <- today$wellid[which.max(today$dtw)]
-  v   <- reactiveValues(msg = "Arizona Dept. of Water Resources")
-  
-  output$groundMap     <- renderLeaflet({ basemap })
-  output$groundGraph   <- renderDygraph({ make_graph(az_time, wellid) })
-  output$groundGraph2  <- renderDygraph({ make_graph2(az_time, wellid) })
-  output$groundPlot    <- renderPlotly({ withdrawals_plot(az_time, sector_total, wellid) })
-  output$groundTable <- renderDT({ make_table(today, az_time, wellid) })
-  output$groundMessage <- renderText(v$msg)
-  output$depthValue <- renderValueBox({
-    valueBox(
-      paste0((well_stats(az_time, wellid)[3])),
-      subtitle = "DEPTH",
-      icon = icon("water"),
-      color = "blue") })
-  output$minValue <- renderValueBox({
-    valueBox(
-      paste0((well_stats(az_time, wellid)[4])),
-      subtitle = "MINMUMUM DEPTH",
-      icon = icon("arrow-up"),
-      color = "green") })
-  output$maxValue <- renderValueBox({
-    valueBox(
-      paste0((well_stats(az_time, wellid)[5])),
-      subtitle = "MAXIMUM DEPTH",
-      icon = icon("arrow-down"),
-      color = "red") })
-  observeEvent(input$groundMap_marker_mouseout, {
-    v$msg <- "Mouse is over: "
-  })
-  
-  observeEvent(input$groundMap_marker_click, {
-    wellid <<- input$groundMap_marker_click$id
-    print(wellid)
-    output$groundGraph <- renderDygraph({ make_graph(az_time, wellid) })
-    
-    # output$groundTable   <- renderDT({ make_table(az_time, wellid) })
-    output$groundGraph2  <- renderDygraph({ make_graph2(az_time, wellid) })
-    output$groundPlot    <- renderPlotly({ withdrawals_plot(az_time, sector_total, wellid) })
-    output$groundTable <- renderDT({ make_table(today, az_time,  wellid) })
-    output$groundMessage <- renderText(v$msg)
-    output$depthValue <- renderValueBox({
-      valueBox(
-        paste0((well_stats(az_time, wellid)[3])),
-        subtitle = "DEPTH",
-        icon = icon("user"),
-        color = "blue") })
-    leafletProxy('groundMap')
-  })
-}
+     # Global variables initialized
+       wellid <- today$wellid[which.max(today$dtw)]
+       v   <- reactiveValues(msg = "Arizona Dept. of Water Resources")
+       
+         output$groundMap     <- renderLeaflet({ basemap })
+         output$groundGraph   <- renderDygraph({ make_graph(az_time, wellid) })
+         output$groundGraph2  <- renderDygraph({ make_graph2(az_time, wellid) })
+         output$groundPlot    <- renderPlotly({ withdrawals_plot(az_time, sector_total, wellid) })
+         output$groundTable <- renderDT({ make_table(today, az_time, wellid) })
+         output$groundMessage <- renderText(v$msg)
+         output$depthValue <- renderValueBox({
+             valueBox(
+                 paste0((well_stats(az_time, wellid)[3])),
+                 subtitle = "DEPTH",
+                 icon = icon("water"),
+                 color = "blue") })
+         output$minValue <- renderValueBox({
+             valueBox(
+                 paste0((well_stats(az_time, wellid)[4])),
+                 subtitle = "MINMUMUM DEPTH",
+                 icon = icon("arrow-up"),
+                 color = "green") })
+         output$maxValue <- renderValueBox({
+             valueBox(
+                 paste0((well_stats(az_time, wellid)[5])),
+                 subtitle = "MAXIMUM DEPTH",
+                 icon = icon("arrow-down"),
+                 color = "red") })
+         observeEvent(input$groundMap_marker_mouseout, {
+             v$msg <- "Mouse is over: "
+           })
+         
+           observeEvent(input$groundMap_marker_click, {
+               wellid <<- input$groundMap_marker_click$id
+               print(wellid)
+               output$groundGraph <- renderDygraph({ make_graph(az_time, wellid) })
+               
+                 # output$groundTable   <- renderDT({ make_table(az_time, wellid) })
+                 output$groundGraph2  <- renderDygraph({ make_graph2(az_time, wellid) })
+                 output$groundPlot    <- renderPlotly({ withdrawals_plot(az_time, sector_total, wellid) })
+                 output$groundTable <- renderDT({ make_table(today, az_time,  wellid) })
+                 output$groundMessage <- renderText(v$msg)
+                 output$depthValue <- renderValueBox({
+                     valueBox(
+                         paste0((well_stats(az_time, wellid)[3])),
+                         subtitle = "DEPTH",
+                         icon = icon("user"),
+                         color = "blue") })
+                 leafletProxy('groundMap')
+               })
+       }
+
+shinyApp(ui, server)
 
 
-
-shinyApp( ui, server )
-
-# 
-# 
-# ui = fluidPage(
-#   theme =  shinytheme("united"),
-#   titlePanel('Arizona Groundwater Dashboard'),
-#   
-#   sidebarPanel(
-#     # textOutput("groundMessage", container = h4),
-#     
-#     
-#     # dygraphOutput('groundGraph'),
-#     plotlyOutput('groundPlot'),
-#     dygraphOutput('groundGraph2')
-#     
-#     
-#   ),
-#   
-#   mainPanel(
-#     leafletOutput('groundMap'),
-#     dygraphOutput('groundGraph')
-#     # plotlyOutput('groundPlot')
-#     # DTOutput("groundTable")
-#   )
-# )
-# 
-# 
-# # Server logic ----
-# server <- function(input, output, session) { 
-#    
-#  wellid <- today$wellid[which.max(today$dtw)]
-#  v   <- reactiveValues(msg = "Arizona Dept. of Water Resources")
-#  
-#  output$groundMap     <- renderLeaflet({ basemap })
-#  output$groundGraph   <- renderDygraph({ make_graph(az_time, wellid) })
-#  output$groundGraph2  <- renderDygraph({ make_graph2(az_time, wellid) })
-#  output$groundPlot    <- renderPlotly({ make_buffer2(az_time, wellid) })
-#  output$groundMessage <- renderText(v$msg)
-# 
-#  observeEvent(input$groundMap_marker_mouseout, {
-#    v$msg <- "Mouse is over: "
-#  })
-#  
-#  observeEvent(input$groundMap_marker_click, {
-#    wellid <<- input$groundMap_marker_click$id
-#    print(wellid)
-#    output$groundGraph <- renderDygraph({ make_graph(az_time, wellid) })
-# 
-#    # output$groundTable   <- renderDT({ make_table(az_time, wellid) })
-#    output$groundGraph2  <- renderDygraph({ make_graph2(az_time, wellid) })
-#    output$groundPlot    <- renderPlotly({ make_buffer2(az_time, wellid) })
-#    output$groundMessage <- renderText(v$msg)
-#    leafletProxy('groundMap')
-#  })
-# }
-# 
-# 
-# shinyApp(ui, server)
-# 
-# 
-# 
-# 
-# 
-# 
 
 
 
--- app/helper2.R
+++ app/helper2.R
@@ -1,26 +1,37 @@
+# library(tidyverse)    # data.frames
+# library(sf)       # Spatial
+# library(data.table)
+# 
+# 
+# # Interactive Data Viz
+# library(leaflet)  # Maps
+# library(dygraphs) # Charts
+# library(plotly)
+# library(DT)       # tables
+# library(rvest)    # webscraping
+# 
+# # Shiny
+# library(dqshiny)    # auto complete
+# library(shiny)       # Starting Reactivity
+# library(shinythemes) # themes
 
-library(tidyverse)    # data.frames
-library(sf)       # Spatial
-library(data.table)
+# ### TEST POINT FOR CLIMATE DATA ####
+# lat = 35.6643
+# lng = -96.91935
+# pt <- data.frame(lat, lng)
+# pt <- sf::st_as_sf(pt,
+#                    coords = c("lng", "lat"),
+#                    crs = 4326)
 
+# library(climateR)
+# 
+# evap <- climateR::getTerraClim(AOI = pt, param = "aet",
+#                                 startDate = "1993-01-01",
+#                                 endDate = "2015-01-01")
 
-# Interactive Data Viz
-library(leaflet)  # Maps
-library(dygraphs) # Charts
-library(plotly)
-library(DT)       # tables
-library(rvest)    # webscraping
-
-# Shiny
-library(dqshiny)    # auto complete
-library(shiny)       # Starting Reactivity
-library(shinythemes) # themes
-library(shinydashboard)
-
-az_time = readRDS('data/az-join-time.rds') 
-az_time$dtw_neg = az_time$dtw*(-1)
-
-sector_total <- readRDS('data/sector_total.rds')
+# 
+# 
+# sector_total <- readRDS('sectors.rds')
 
 # Join County Data with 
 today_pts = function(time_data){
@@ -42,22 +53,22 @@ make_graph  = function(az_time, wellid){
     data.frame()
   
   subset = subset[!duplicated(subset$date),]
-
+  
   rownames(subset) = subset$date
-      
+  
   dygraph(data = dplyr::select(subset,dtw),
-            main = paste0("WELL ", subset$wellid[1]),
-            ylab = 'DEPTH TO WATER (ft)',
-            xlab = 'DATE') %>%
+          main = paste0("WELL ", subset$wellid[1]),
+          ylab = 'DEPTH TO WATER (ft)',
+          xlab = 'DATE') %>%
     dyAxis('y', valueRange = c(100 + max(subset$dtw), 0)) %>% 
-      dyHighlight(highlightCircleSize = 4,
-                  highlightSeriesBackgroundAlpha = 0.6,
-                  highlightSeriesOpts = list(strokeWidth = 4)) %>%
-      dyOptions(includeZero = TRUE,
-                colors = c("navy"), strokeWidth = 3)
-       
-                 # stackedGraph = TRUE
-
+    dyHighlight(highlightCircleSize = 4,
+                highlightSeriesBackgroundAlpha = 0.6,
+                highlightSeriesOpts = list(strokeWidth = 4)) %>%
+    dyOptions(includeZero = TRUE,
+              colors = c("navy"), strokeWidth = 3)
+  
+  # stackedGraph = TRUE
+  
 }
 
 
@@ -77,18 +88,29 @@ basemap = function(az_time){
     st_transform(4326)
   
   # read in AMA shapefiles
-  ama = read_sf('az-ama-shps/Act_Man_Areas.shp') %>% 
-    st_transform(4326) %>% 
-    st_cast("MULTIPOLYGON")
+  # ama = read_sf('app/shps/Act_Man_Areas.shp') %>% 
+  #   st_transform(4326) %>% 
+  #   st_cast("MULTIPOLYGON")
   # ama = ama %>% st_transform(4326)
   
   col3 = RColorBrewer::brewer.pal(9,"YlGnBu")
   pals3 = colorBin(col3, domain = 1:8)
   
   leaflet(data = az_spatial) %>%
-    addProviderTiles(providers$CartoDB.Positron) %>%
-    addMapPane("wells", zIndex = 420) %>% 
-    addMapPane("AMAs", zIndex = 410) %>% 
+    addProviderTiles(providers$OpenStreetMap, group = "Topographic") %>%
+    addProviderTiles(providers$Esri.WorldImagery, group = "Satellite") %>%
+    addProviderTiles(providers$Esri.WorldShadedRelief, group = "Relief") %>%
+    addScaleBar("bottomleft") %>%
+    setView(-112,34,7) %>%
+    addMeasure(
+      position = "bottomleft",
+      primaryLengthUnit = "feet",
+      primaryAreaUnit = "sqmiles",
+      activeColor = "red",
+      completedColor = "green" ) %>%
+    leafem::addMouseCoordinates() %>% 
+    addMapPane("wells", zIndex = 420) %>%
+    addMapPane("AMAs", zIndex = 410) %>%
     addPolygons(data = az,
                 fillColor = 'transparent', 
                 opacity = 7,
@@ -110,22 +132,22 @@ basemap = function(az_time){
                      layerId = ~wellid,
                      options = pathOptions(pane = "wells"),
                      group = "WELLS") %>% 
-    addPolygons(data = ama,
-                fillColor  = ~pals3(OBJECTID),
-                fillOpacity = .4,
-                opacity = .7,
-                color = 'black',
-                weight = 3,
-                label = ~MAP_LABEL,
-                options = pathOptions(pane = "AMAs"),
-                group = 'AMA') %>% 
+    # addPolygons(data = ama,
+    #             fillColor  = ~pals3(OBJECTID),
+    #             fillOpacity = .4,
+    #             opacity = .7,
+    #             color = 'black',
+    #             weight = 3,
+    #             label = ~MAP_LABEL,
+    #             options = pathOptions(pane = "AMAs"),
+    #             group = 'AMA') %>% 
     addLegend("bottomright",
               pal = pal,
               values = ~dtw,
               title = 'Depth to water (ft)',
               opacity = 1) %>% 
-    addLayersControl(overlayGroups = c("WELLS", 'AMA'),
-                     options = layersControlOptions(collapsed = FALSE)) 
+    addLayersControl(overlayGroups = c("WELLS"), options = layersControlOptions(collapsed = FALSE),
+                     baseGroups = c('Topographic', "Relief", 'Satellite'))
 }
 
 zoom_to_well = function(map, today, well){
@@ -154,6 +176,7 @@ zoom_to_well = function(map, today, well){
 ## Build time series table
 make_table = function(today, az_time, wellid){
   tmp1 = today %>% filter(wellid == !!wellid)
+  # tmp1 = today %>% filter(wellid == 2)
   # tmp1 = az_time %>% filter(wellid == wellid)
   # Filter todays data to wellid time series record
   tmp2 = az_time %>% filter(wellid == tmp1$wellid) %>%
@@ -240,8 +263,7 @@ withdrawals_plot <- function(az_time, sector_total, wellid){
     layout(legend = list(
       orientation = "h",
       x = 0.4, y = 1.1,
-      itemclick = "toggleothers"))  %>% 
-    config(displayModeBar = FALSE)
+      itemclick = "toggleothers"))
   # style(hoverlabel = label) %>% 
   # layout(font = font, 
   #        yaxis = list(fixedrange = TRUE)) %>% 
@@ -249,10 +271,6 @@ withdrawals_plot <- function(az_time, sector_total, wellid){
 }
 
 # withdrawals_plot(az_time, sector_total, 5301)
-
-
-
-
 
 
 
